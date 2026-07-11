9b5c24fa55c2d4fbd27a68a2ad6f79694fb71d87
emelieChukwu <ezehigc@gmail.com>
2026-07-11 01:30:22 +0100

    docker groovy changes

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,10 +1,7 @@
 #!/user/bin/env groovy
 
-def call(){
-    echo "Building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]){
-        sh 'docker build -t emeliechukwu/demo-app:jma-2.0 .'
-        sh 'echo $PASS | docker login -u $USER --password-stdin'
-        sh 'docker push emeliechukwu/demo-app:jma-2.0'
-    }
+import com.example.Docker
+
+def call(String imageName){
+    return new Docker(this).buildDockerImage(imageName)
 }
